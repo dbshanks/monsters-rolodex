6c09ac04abac44f2b4b3c1faf606b91e84dee064
FragileGlass <derekbshanks@gmail.com>
2019-09-23 22:19:10 -0300

    Updates

---
+++ precache-manifest.e124c9dad33c9685366487b3264670c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e1576cb2d47ca7c4065eba799a0d66",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eaece7af9ac650726563",
+    "url": "/monsters-rolodex/static/css/main.9cd37619.chunk.css"
+  },
+  {
+    "revision": "68d48b2bf5f52e884d73",
+    "url": "/monsters-rolodex/static/js/2.62c42cef.chunk.js"
+  },
+  {
+    "revision": "eaece7af9ac650726563",
+    "url": "/monsters-rolodex/static/js/main.f202fefa.chunk.js"
+  },
+  {
+    "revision": "cd254c0526ecfbaa3b08",
+    "url": "/monsters-rolodex/static/js/runtime-main.a979b867.js"
+  }
+]);
